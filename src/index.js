eeb8a0581c65c99f0311df6318cab2928439081d
Pier Bover <pierbover11@gmail.com>
2021-04-07 18:22:39 -0500

    removed back() export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export {currentRoute, currentPath, navigate, back, addQueryParamsToUrl} from './router.js';
+export {currentRoute, currentPath, navigate, addQueryParamsToUrl} from './router.js';
 export {default as Router} from './Router.svelte';
 export {default as Route} from './Route.svelte';
 export {default as link} from './actions/link.js';
